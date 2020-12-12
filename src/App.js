8e2ee4dc8f2d5a47d936e8d98ada89afecde29ba
stevensam0109 <steven.sam.0109@outlook.com>
2020-12-12 12:10:48 +0900

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-import Header from './components/Header';
-import Search from './components/Search';
-import MovieDetail from './components/MovieDetail';
-import MovieList from './components/MovieList';
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/" component={MovieList} />
-      <Route path="/movies/:movieID" component={MovieDetail} />
-    </Switch>
-  )
-}
+    <div></div>
+  );
+};
 
 export default App;
